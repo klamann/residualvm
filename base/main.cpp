bgK/scummvm

Allow overriding the user configuration file

base/main.cpp
@@ -398,6 +398,8 @@ extern "C" int scummvm_main(int argc, const char * const argv[]) {
 		ConfMan.loadDefaultConfigFile();
 	}
 
+	ConfMan.loadOverrideFile("scummvm-override.ini");
+
 	// Update the config file
 	ConfMan.set("versioninfo", gScummVMVersion, Common::ConfigManager::kApplicationDomain);
 

klamann/residualvm

base/main.cpp
@@ -397,6 +397,8 @@ extern "C" int scummvm_main(int argc, const char * const argv[]) {
 		ConfMan.loadDefaultConfigFile();
 	}
 
+	ConfMan.loadOverrideFile("residualvm-override.ini");
+
 	// Update the config file
 	ConfMan.set("versioninfo", gScummVMVersion, Common::ConfigManager::kApplicationDomain);
 